400fff175b8f9a025902f9b379e9ed2ca36809d3
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2020-03-05 23:11:45 -0300

    refactor(styles): removing unsed styled component

--- src/styles/SharedStyles.tsx
+++ src/styles/SharedStyles.tsx
@@ -10,17 +10,6 @@ interface BlueGradientHorizontalProps {
   height?: string
 }
 
-export const BlueGradientHorizontal = styled.div<BlueGradientHorizontalProps>`
-  background: ${({
-    theme: {
-      colors: { blue }
-    }
-  }) =>
-    `linear-gradient(90deg, ${blue[51]} 0%, ${blue[41]} 50%, ${blue[21]} 100%);`};
-
-  height: ${({ height }) => height || '5px'};
-`
-
 export const FullHeightWrapper = styled.div`
   height: 100vh;
   display: flex;
